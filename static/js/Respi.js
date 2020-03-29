336d496bc144e30c40b56ef644e53e04f2a7927a
Maciej Such <msuch92@gmail.com>
2020-03-29 22:38:38 +0200

    wstepnie ustawiona strona

--- static/js/Respi.js
+++ static/js/Respi.js
@@ -1,10 +1,26 @@
 const btn_plus = document.querySelector('.plus_button');
 const btn_minus = document.querySelector('.minus_button');
+const btn_plus_rate = document.querySelector('.plus_rate');
+const btn_minus_rate = document.querySelector('.minus_rate');
+const btn_start = document.querySelector('.button_start');
 
 btn_plus.addEventListener('click', function () {
-  console.log('Klinąłeś plus')
+  console.log('ac inc')
 });
 
 btn_minus.addEventListener('click', function () {
-  console.log('Klinąłeś minus')
+  console.log('ac dec')
+})
+
+
+btn_plus_rate.addEventListener('click', function () {
+  console.log('rate inc')
+});
+
+btn_minus_rate.addEventListener('click', function () {
+  console.log('rate dec')
+})
+
+btn_start.addEventListener('click', function () {
+  console.log('START')
 })
